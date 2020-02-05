ec639a99e72e149b53395e445c75a7039d64f456
hunterxx0 <azouzimhamed@gmail.com>
2020-02-05 14:43:57 +0000

    first

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - printf
+* Return: (0)
+*/
+int main(void)
+{
+printf("Size of a char :"sizeof(char)" byte(s)");
+printf("Size of an int: "sizeof(int)" byte(s)");
+printf("Size of a long int: "sizeof(unsigned int)" byte(s)");
+printf("Size of a long long int: "sizeof(long)" byte(s)");
+printf("Size of a float: "sizeof(float)" byte(s)");
+return (0);
+}

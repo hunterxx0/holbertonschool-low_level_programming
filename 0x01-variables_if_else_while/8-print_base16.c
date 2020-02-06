4c2bb7d90c8a8681ecd62963637af414c63ce482
hunterxx0 <azouzimhamed@gmail.com>
2020-02-06 17:28:33 +0000

    first

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - base16
+* Return: (0)
+*/
+int main(void)
+{
+int n;
+char c;
+n = 48;
+for (n = 48; n <= 57; n++)
+{
+putchar(c);
+}
+for (c = 97; c <= 102; c++)
+putchar('\n');
+return (0);
+}

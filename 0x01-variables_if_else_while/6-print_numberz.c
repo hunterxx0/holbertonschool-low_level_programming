be697b1787d39fecff1e53dfd0ae154f5ed7070c
hunterxx0 <azouzimhamed@gmail.com>
2020-02-06 13:37:30 +0000

    first

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - alphabet
+* Return: (0)
+*/
+int main(void)
+{
+int c;
+c = 0;
+for (c = 0; c <= 9; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}

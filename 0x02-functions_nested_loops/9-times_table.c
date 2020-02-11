4534fdff76c25f217940d3cc28d5f81d04522c02
hunterxx0 <azouzimhamed@gmail.com>
2020-02-11 23:58:57 +0000

    first

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,40 +5,30 @@
 */
 void times_table(void)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
+int a, b, c;
+for (a = 0; a < 10; a++)
 {
-b = 0;
-c = 0;
-while (b < 10)
+for (b = 0; b < 10; b++)
 {
+c = a * b;
 if (c <= 9)
 {
-_putchar (c + '0');
-if (b < 9)
+if (b != 0)
 {
-_putchar (',');
-_putchar (' ');
-_putchar (' ');
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
 }
-c = c + a;
-b++;
+_putchar(c + '0');
 }
 else
 {
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar (',');
-_putchar (' ');
-}
-c = c + a;
-b++;
+_putchar(',');
+_putchar(' ');
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
 }
 }
-_putchar ('\n');
+_putchar('\n');
 }
 }

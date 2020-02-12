7b6545a6f0e1fdf20a917535c7969a7cb89de10d
hunterxx0 <azouzimhamed@gmail.com>
2020-02-12 19:28:26 +0000

    adv

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,11 @@ int main(void)
 unsigned long int a = 0, b = 1, i, c;
 for (i = 1; i <= 50; i++)
 {
-printf("%lu, ", b);
+printf("%lu", b);
+if (i != 50)
+{
+printf(", ");
+}
 c = a + b;
 a = b;
 b = c;

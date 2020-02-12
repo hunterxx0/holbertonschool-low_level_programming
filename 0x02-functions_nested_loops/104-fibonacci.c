684b41a41cd85f87964fc8e2ddfeb4af194620cc
hunterxx0 <azouzimhamed@gmail.com>
2020-02-12 22:03:21 +0000

    adv

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -5,19 +5,43 @@
 */
 int main(void)
 {
-unsigned long int a = 0, b = 1, c;
-int i;
-for (i = 1; i <= 100; i++)
+long a, a1, a2, b, b1, b2, c, c1, c2;
+long d = 10000000000;
+int i = 1;
+a = 0;
+b = 1;
+for (i = 0; i < 91; i++)
 {
-c = a + b;
-printf("%lu", c);
-if (i < 100)
-{
-printf(", ");
-}
+c =  b + a;
 a = b;
 b = c;
+printf("%ld, ", c);
 }
-printf("\n");
+a1 = a / d;
+a2 = a % d;
+b1 = b / d;
+b2 = b % d;
+for (; i < 98; i++)
+{
+c1 = a1 + b1;
+c2 = a2 + b2;
+if (c2 > d)
+{
+c2 = c2 % d;
+c1++;
+}
+if (i != 97)
+{
+printf("%ld%ld, ", c1, c2);
+a1 = b1;
+a2 = b2;
+b1 = c1;
+b2 = c2;
+}
+else
+{
+printf("%ld%ld\n", c1, c2);
+}
+}
 return (0);
 }

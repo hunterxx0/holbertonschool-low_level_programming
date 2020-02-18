a11d5384b4169510c42a5e324bd754648d0dac42
hunterxx0 <azouzimhamed@gmail.com>
2020-02-18 11:36:22 +0000

    first

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* puts_half(:)? (- other char)?
+*
+* @str: input string
+* Return: 0 or 1
+*/
+void puts_half(char *str)
+{
+int i = 0, j;
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+for (j = i / 2; j < i; j++)
+{
+_putchar(str[j]);
+}
+}
+else
+{
+for (j = (i - 1) / 2; j <= i; j++)
+{
+_putchar(str[j]);
+}
+}
+_putchar('\n');
+}

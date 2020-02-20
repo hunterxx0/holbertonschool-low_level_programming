26eff4d1bb8276dae0f8a26516a834366da51ac2
hunterxx0 <azouzimhamed@gmail.com>
2020-02-20 14:27:22 +0000

    first

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,17 +10,19 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i, j;
-for (i = 0; dest[i] != '\0'; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
 }
 j = 0;
 while (src[j] != '\0' && j < n)
 {
 dest[j] = src[j];
-i++;
 j++;
 }
-if (n > i)
-dest[i + 1] = '\0';
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+} 
 return (dest);
 }

f85a69c2ba5810f8935b5e752b863e6087b3d330
hunterxx0 <azouzimhamed@gmail.com>
2020-02-20 22:13:22 +0000

    first

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* *cap_string(:)? (- rev arr)?
+*
+* @x: input array
+* Return: 0
+*/
+char *cap_string(char *x)
+{
+int i, j;
+char s[] = {'\n', '\t', ' ', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+for (i = 0; x[i] != '\0'; i++)
+{
+for (j = 0; j <= 13; j++)
+{
+if (x[i] == s[j] && (x[i + 1] >= 'a' && x[i + 1] <= 'z'))
+{
+x[i + 1] = x[i + 1] - 32;
+}
+}
+}
+return (x);
+}

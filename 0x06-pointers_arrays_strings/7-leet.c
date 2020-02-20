9ebd892d27fe754b4d82c6af9e4b3b7b17150e41
hunterxx0 <azouzimhamed@gmail.com>
2020-02-20 22:45:00 +0000

    first

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,23 @@
 /**
 * *leet(:)? (- switch)?
 *
-* @x: input array
+* @swt: input array
 * Return: 0
 */
-char *leet(char *x)
+char *leet(char *swt)
 {
 int i, j;
 char l[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 char s[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-for (i = 0; x[i] != '\0'; i++)
+for (i = 0; swt[i] != '\0'; i++)
 {
 for (j = 0; j < 10; j++)
 {
-if (x[i] == l[j])
+if (swt[i] == l[j])
 {
-x[i] = s[j];
+swt[i] = s[j];
 }
 }
 }
-return (x);
+return (swt);
 }

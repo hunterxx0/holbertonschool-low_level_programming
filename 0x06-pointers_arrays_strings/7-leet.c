e4b384923803d92c5e0f266dd4b01c46a43339a8
hunterxx0 <azouzimhamed@gmail.com>
2020-02-20 23:37:21 +0000

    first

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,11 +8,11 @@
 char *leet(char *swt)
 {
 int i, j;
-char l[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-char s[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+char l[] = "aAeEoOtTlL";
+char s[] = "4433007711";
 for (i = 0; swt[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; l[j] != '\0'; j++)
 {
 if (swt[i] == l[j])
 swt[i] = s[j];

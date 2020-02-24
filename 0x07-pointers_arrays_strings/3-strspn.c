37a8b60a900a0a27f0dd135a2aca205d80e2661d
hunterxx0 <azouzimhamed@gmail.com>
2020-02-24 19:13:29 +0000

    first

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+* _strspn(:)? (- accept)?
+*
+* @s: output char
+* @a: input source char
+* Return: dest
+*/
+unsigned int _strspn(char *s, char *a)
+{
+unsigned int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; a[j] != '\0'; j++)
+{
+ if (s[i] == a[j])
+break;
+}
+if (s[i] != a[j])
+break;
+}
+return (i);
+}

c612f4b13ffa3ac1298aab1302e5da6830cd34f8
hunterxx0 <azouzimhamed@gmail.com>
2020-02-26 21:17:33 +0000

    first

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+/**
+ * check(:)? (- check prime)?
+ *
+ * @n: input int
+ * @i: input int
+ * Return: n
+ */
+int check(int n, int i)
+{
+	if (i == n)
+	{
+		return (-1);
+	}
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (check(n, i + 1));
+}
+
+/**
+* _sqrt_recursion(:)? (- root)?
+*
+* @n: input int
+* Return: c
+*/
+int _sqrt_recursion(int n)
+{
+	int c = check(n, 1);
+	if (n == 1)
+		return (1);
+	else
+		return (c);
+}

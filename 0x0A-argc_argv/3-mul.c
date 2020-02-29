9b795d612e59544f4616871f3dd41d2f46950560
hunterxx0 <azouzimhamed@gmail.com>
2020-02-29 16:37:33 +0000

    first

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,10 +9,15 @@
 */
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc < 2 && (argv[1] > 'a' && argv[1] < 'z'))
 	{
 		printf("Error\n");
 			return (1);
+	}
+	if (argv[2] > 'a' && argv[2] < 'z')
+	{
+		printf("Error\n");
+return (1);
 	}
 	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);

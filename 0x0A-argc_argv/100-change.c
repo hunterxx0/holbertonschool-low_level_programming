9c3793cb52248f2755d726477ee3867f19243af0
hunterxx0 <azouzimhamed@gmail.com>
2020-02-29 18:23:26 +0000

    first

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
 	int sum = 0, n, i = 0;
 	int z[] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
-        {
-                printf("Error\n");
-		return (1);
-        }
+if (argc != 2)
+{
+printf("Error\n");
+return (1);
+}
 	if (atoi(argv[1]) < 0)
 	{
 		printf("0\n");

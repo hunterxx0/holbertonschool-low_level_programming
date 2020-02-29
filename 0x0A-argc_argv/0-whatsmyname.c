a2a793f4ba32fbe5582f0a8ec4204ff6f764e369
hunterxx0 <azouzimhamed@gmail.com>
2020-02-29 16:03:53 +0000

    first

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main(:)? (- argc argv)?
+*
+* @argc: input int
+* @argv: input char
+* Return: argv
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0])
+		return(0);
+}

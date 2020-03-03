2bcce39df39f81f29c075fafb2ce506902a0ae59
hunterxx0 <azouzimhamed@gmail.com>
2020-03-03 14:19:19 +0000

    first

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,11 @@ char *str_concat(char *s1, char *s2)
 	;
 	for (k = 0; s2[k] != '\0'; k++)
 	;
-	if (j == 0 && k == 0)
-		return (0);
 	k--;
 	j--;
-	z = malloc(sizeof(char) * (k + j + 1));
+        z = malloc(sizeof(char) * (k + j + 1));
+	if ((j == 0 && k == 0) || z == NULL)
+		return (0);
 	for (i = 0; i <= k + j + 1; i++)
 	{
 		if (i <= j)

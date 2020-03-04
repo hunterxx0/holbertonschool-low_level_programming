162b8e1df7f1698e84261b63fd7382a87d7003ee
hunterxx0 <azouzimhamed@gmail.com>
2020-03-04 14:37:22 +0000

    first

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,22 +9,28 @@
 */
 int **alloc_grid(int w, int h)
 {
-	int **z, *p;
-	int i, j, l;
+	int **z;
+	int i, j;
+
 
 	if (w <= 0 || h <= 0)
-		return (0);
-	l = sizeof(int *) * h + sizeof(int) * w * h;
-	z = (int **)malloc(l);
-	p = (int *)(z + h);
-	for (i = 0; i < h; i++)
-		z[i] = (p + w * i);
-	for (i = 0; i < w; i++)
-	{
-		for (j = 0; j < h; j++)
+		return (NULL);
+	z = (int**)malloc(sizeof(int) * h);
+		if (z == NULL)
+			return (NULL);
+		for (i = 0; i < h; i++)
 		{
-			*((z + i) + j) = 0;
+			z[i] = malloc(sizeof(int) * w);
+			if (z[i] == NULL)
+			{
+				free(z[i]);
+				free(z);
+				return (NULL);
+			}
+			for (j = 0; j < h; j++)
+			{
+				z[i][j] = 0;
+			}
 		}
-	}
-	return (z);
+		return (z);
 }

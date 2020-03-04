a99331c2c4ec4945d3b3ae7231af11c08df73bae
hunterxx0 <azouzimhamed@gmail.com>
2020-03-04 16:06:18 +0000

    first

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* alloc_grid? (- concat)?
+*
+* @g: input int size
+* @h: input int size
+* Return: z
+*/
+void free_grid(int **g, int h)
+{
+	int i;
+	for (i = 0; i < h; i++)
+	{
+		free(z[i]);
+	}
+	free(z);
+}

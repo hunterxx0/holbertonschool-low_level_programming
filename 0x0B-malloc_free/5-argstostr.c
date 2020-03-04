f2512012603ce2682c2fedb296d227bbd984136c
hunterxx0 <azouzimhamed@gmail.com>
2020-03-04 19:21:07 +0000

    first

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -3,23 +3,24 @@
 /**
 * argstostr(:)? (- printit)?
 *
-* @argc: input int
-* @argv: input char
+* @ac: input int
+* @av: input char
 * Return: argv
 */
 char *argstostr(int ac, char **av)
 {
-	int i, j, x = 0,sum = 0;
+	int i, j, sum = 0;
 	char *z;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
+		for (x = 0; av[i][x]; x++)
 			sum++;
 		sum++;
 	}
+	x = 0;
 	z = malloc(sizeof(char) * (sum + 1));
 	if (z == NULL)
 		return (NULL);
@@ -27,8 +28,8 @@ char *argstostr(int ac, char **av)
 	{
 		for (j = 0; av[i][j]; j++)
 		{
-	z[x] = av[i][j];
-	x++;
+			z[x] = av[i][j];
+			x++;
 		}
 		z[x] = '\n';
 		x++;

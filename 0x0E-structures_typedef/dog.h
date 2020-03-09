3a5e4691fca459dd070aa2b2c236ce50edc6d517
hunterxx0 <azouzimhamed@gmail.com>
2020-03-09 18:57:34 +0000

    first

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+/**
+ * struct dog - strct.
+ *
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* HOLBERTON_H */

b80ea02c286e0e42ce0810106959940485945768
hunterxx0 <azouzimhamed@gmail.com>
2020-03-09 22:44:03 +0000

    first

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,33 +14,30 @@ dog_t *new_dog(char *n, float a, char *o)
 	dog_t *p;
 	char *na;
 	char *ow;
-	int i, j, c = 0;
+	int i = 0, j = 0, c = 0;
 
-	if (n == NULL)
-		n = "";
-	if (o == NULL)
-		o = "";
-	p = (struct dog *)malloc(sizeof(struct dog));
+	if (n == NULL || o == NULL)
+		return (NULL);
+	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
-	for (i = 0; n[i] != '\n'; i++)
-		;
-	for (j = 0; o[j] != '\n'; j++)
-		;
-	na = malloc(sizeof(char) * i);
+	while (n[i])
+		i++;
+	while (o[j])
+		j++;
+	na = malloc(sizeof(char) * (i + 1));
 	if (na == NULL)
 	{
-		free(na);
 		free(p);
 		return (NULL);
 	}
 	for (c = 0; c < i; c++)
 		na[c] = n[c];
 	na[c] = '\0';
-	ow = malloc(sizeof(char) * j);
+	ow = malloc(sizeof(char) * (j + 1));
 	if (ow == NULL)
 	{
 		free(ow);

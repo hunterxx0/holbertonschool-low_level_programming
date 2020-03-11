df9f9be094aa0aa7b77da4fb1c266b71948ddb53
hunterxx0 <azouzimhamed@gmail.com>
2020-03-11 20:23:47 +0000

    first

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main(:)? (- argc argv)?
+ *
+ * @ac: number of arguments
+ * @av: arguments
+ *
+ * Return: r
+ */
+int main(int ac, char *av[])
+{
+	if (ac != 2)
+	{
+		printf("Error\n");
+			exit(1);
+	}
+
+	if(strlen(av[1]) != 2)
+	{
+		printf("Error\n");
+                exit(2);
+	}
+	printf("%x\n", atoi(av[1]));
+	return (0);
+}

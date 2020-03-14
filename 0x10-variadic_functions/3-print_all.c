26366722fa637933c1b1f996f48731a187d89d93
hunterxx0 <azouzimhamed@gmail.com>
2020-03-14 17:01:25 +0000

    first

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,49 +1,5 @@
 #include "variadic_functions.h"
-/**
- * print_c? (- print c)?
- *
- * @ap: input format
- * Return:
- */
-void print_c(va_list ap)
-{
-	printf("%c", va_arg(ap, int));
-}
-/**
- * print_i? (- print int)?
- *
- * @ap: input format
- * Return:
- */
-void print_i(va_list ap)
-{
-	printf("%d", va_arg(ap, int));
-}
-/**
- * print_f? (- print f)?
- *
- * @ap: input format
- * Return:
- */
-void print_f(va_list ap)
-{
-	printf("%f", va_arg(ap, double));
-}
-/**
- * print_s? (- print s)?
- *
- * @ap: input format
- * Return:
- */
-void print_s(va_list ap)
-{
-	char *str = va_arg(ap, char *);
-
-	if (str != NULL)
-		printf("%s", str);
-	else
-		printf("(nil)");
-}
+#include "funct.h"
 /**
  * print_all? (- print a)?
  *
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+void print_c(va_list ap);
+void print_s(va_list ap);
+void print_f(va_list ap);
+void print_i(va_list ap);
 /**
  * struct func - print
  * @c: char
@@ -18,4 +22,5 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
 #endif /* FUNCTION_POINTERS_H */

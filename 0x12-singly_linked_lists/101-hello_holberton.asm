d99d40c6e9b74e8b3366abdabfbd7bd1a071ffc0
hunterxx0 <azouzimhamed@gmail.com>
2020-03-21 11:40:19 +0000

    first

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .text
+
+	    global main
+
+main:
+
+
+	        mov     rdx,msglen
+	        mov     rsi,msg
+	        mov     rax,1
+	        mov     rdi,1
+	        syscall
+		mov	rdi,0
+	        mov     rax,60
+	        syscall
+
+section .data
+
+	msg     db      "Hello, Holberton",0xa
+	msglen     equ     $ - msg

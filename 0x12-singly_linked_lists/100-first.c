4b1f696044e97ba0e2dfc420d7eb36e8bae44795
hunterxx0 <azouzimhamed@gmail.com>
2020-03-21 11:31:28 +0000

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * free_list? (- free list)?
+ *
+ * @h: input format
+ * Return:
+ */
+void start (void)
+{
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}

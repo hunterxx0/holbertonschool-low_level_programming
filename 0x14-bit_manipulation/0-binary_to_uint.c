116274ea00da34053b3300e02c71bb5e3ac8f6a4
hunterxx0 <azouzimhamed@gmail.com>
2020-03-26 13:10:47 +0000

    first

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,54 +1,33 @@
 #include "holberton.h"
-/**
- * rev_string(:)? (- rev string)?
- *
- * @s: input string
- * Return:
- */
-void rev_string(char *s)
-{
-	char k;
-	int i = 0, j, l;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	l = i - 1;
-	for (j = 0 ; j < (i / 2); j++)
-	{
-		k = s[l];
-		s[l] = s[j];
-		s[j] = k;
-		l--;
-	}
-}
+#include <stdio.h>
 /**
  * binary_to_uint? (- binary to unsg int)?
  *
- * @b: input head
+ * @a: input head
  * Return: x
  */
-unsigned int binary_to_uint(const char *b)
+unsigned int binary_to_uint(const char *a)
 {
 	unsigned int x = 1, i;
+	int l;
 	int z[64];
-	char *a = strdup(b);
 
-	if (b == NULL)
+	if (a == NULL)
 		return (0);
 	for (i = 0; i < 64; i++)
 	{
 		z[i] = x;
 		x *= 2;
 	}
-	rev_string(a);
+	for (l = 0; a[l]; l++)
+		;
+	l--;
 	x = 0;
-	for (i = 0; a[i]; i++)
+	for (i = 0; l >=0; i++, l--)
 	{
-		if (a[i] != '0' && a[i] != '1')
+		if (a[l] != '0' && a[l] != '1')
 			return (0);
-		if (a[i] == '1')
+		if (a[l] == '1')
 			x += z[i];
 	}
 	return (x);

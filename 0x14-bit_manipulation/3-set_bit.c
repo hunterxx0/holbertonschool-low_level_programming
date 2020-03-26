4a277dee828e70faee99dcb34e7c6f2e98234cd1
hunterxx0 <azouzimhamed@gmail.com>
2020-03-26 16:28:09 +0000

    first

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,30 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * rev_str(:)? (- rev string)?
- *
- * @s: input string
- * Return: 0 or 1
- */
-void rev_str(char *s)
-{
-	char k;
-	int i = 0, j, l;
-
-	while (s[i] != '\0')
-	{
-		i++;
-	}
-	l = i - 1;
-	for (j = 0 ; j < (i / 2); j++)
-	{
-		k = s[l];
-		s[l] = s[j];
-		s[j] = k;
-		l--;
-	}
-}
-/**
  * set_bit? (- bit at idx)?
  *
  * @n: input number
@@ -35,10 +11,9 @@ int set_bit(unsigned long int *n, unsigned int i)
 {
 	unsigned long int k;
 
- 	if (i > 65 || !n)
+	if (i > 65 || !n)
 		return (-1);
 	k = 1 << i;
 	*n |= k;
 	return (1);
-
 }

bccdd1a5b5b2706864dc662cb677e2560efde0ea
hunterxx0 <azouzimhamed@gmail.com>
2020-03-26 15:23:03 +0000

    first

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -34,10 +34,10 @@ void rev_str(char *s)
 int get_bit(unsigned long int n, unsigned int i)
 {
 	unsigned long int c = 4611686018427387904, x = 0, j = 0, l = 0;
-	char z[100];
+	char z[500];
 
 	c <<= 1;
-	while (j < 100)
+	while (j < 500)
 	{
 		z[j] = '0';
 		j++;
@@ -65,6 +65,7 @@ int get_bit(unsigned long int n, unsigned int i)
 	l--;
 	if (i > l || i > 32)
 		return (-1);
+
 	return (z[i] - '0');
 
 }

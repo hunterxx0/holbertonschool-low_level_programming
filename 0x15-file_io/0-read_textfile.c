ee3be562f89619d04dfe19041cb6012ae01b1078
hunterxx0 <azouzimhamed@gmail.com>
2020-03-31 17:13:45 +0000

    first

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,39 +8,23 @@
  */
 ssize_t read_textfile(const char *f, size_t l)
 {
-	ssize_t x = 0, len, w = 0;
+	ssize_t w = 0;
 	int fd = 0, r = 0;
-	char c, end = '\n';
+	char *c = malloc(l);
 
-	if (!f)
+	if (!f || !l)
 		return (0);
-	len = (ssize_t)l;
 	fd = open(f, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	while (x < len)
-	{
-		r = read(fd, &c, sizeof(c));
-		if (r == -1)
-			return (0);
-		if (r != 0)
-		{
-			w = write(1, &c, 1);
-			if (w == -1)
-				return (0);
-			x++;
-		}
-		if (r == 0 && x < len)
-		{
-			w = write(1, &end, 1);
-			if (w == -1)
-				return (0);
-			x++;
-			break;
-		}
-	}
-	close(fd);
-	if (x > len)
+	r = read(fd, c, l);
+	if (r == -1)
 		return (0);
-	return (x);
+	w = write(1, c, r);
+	if (w == -1)
+		return (0);
+
+	free(c);
+	close(fd);
+	return (w);
 }

b9ac83796618aed641589ea56be6b66a5a11b0ff
hunterxx0 <azouzimhamed@gmail.com>
2020-03-31 15:01:43 +0000

    first

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,8 +9,8 @@
 ssize_t read_textfile(const char *f, size_t l)
 {
 	ssize_t x = 0, len, w = 0;
-	int fd, b;
-	char c;
+	int fd = 0, r = 0;
+	char c, end = '\n';
 
 	if (f == NULL)
 		return (0);
@@ -18,22 +18,26 @@ ssize_t read_textfile(const char *f, size_t l)
 	fd = open(f, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	while ((b = read(fd, &c, sizeof(c))) > 0 && x < len)
+	while (x < len)
 	{
-		if (b == -1)
+		r = read(fd, &c, sizeof(c));
+		if (r == -1)
 			return (0);
-		write(1, &c, 1);
-		if (w == -1)
-			return (0);
-		x++;
-	}
-	if (b == 0 && x < len)
-	{
-		c = '\n';
-		w = write(1, &c, 1);
-		if (w == -1)
-			return (0);
-		x++;
+		if (r != 0)
+		{
+			w = write(1, &c, 1);
+			if (w == -1)
+				return (0);
+			x++;
+		}
+		if (r == 0 && x < len)
+		{
+			w = write(1, &end, 1);
+			if (w == -1)
+				return (0);
+			x++;
+			return (x);
+		}
 	}
 			close(fd);
 	return (x);

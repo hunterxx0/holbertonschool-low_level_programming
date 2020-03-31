06bec7021db5bcf6eb33f9b36c537225083c9016
hunterxx0 <azouzimhamed@gmail.com>
2020-03-31 17:52:08 +0000

    first

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * read_textfile? (- read file)?
+ * create_file? (- create file)?
  *
  * @f: input file
  * @tc: printed size
@@ -13,14 +13,13 @@ int create_file(const char *f, char *tc)
 
 	if (!f)
 		return (0);
-	fd = open(f, O_CREAT | O_WRONLY, 0600);
+	fd = open(f, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 	if (fd == -1)
 		return (-1);
 	if (tc)
 	{
 	while (tc[i])
 	i++;
-	i--;
 	w = write(fd, tc, i);
 	if (w == -1)
 		return (-1);

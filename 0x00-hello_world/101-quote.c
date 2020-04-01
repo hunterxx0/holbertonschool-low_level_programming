0ebd59642289d7d189e5541216e8c207308fa3eb
hunterxx0 <azouzimhamed@gmail.com>
2020-04-01 13:41:18 +0000

    adv

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
+ * _strlen(:)? (- long)?
+ *
+ * @s: in char
+ * Return: 0 or 1
+ */
+int _strlen(char *s)
+{
+	int i;
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
 * main - write
 * Return: (1)
 */
 int main(void)
 {
-write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(string));
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int l = _strlen(s);
+
+write(STDERR_FILENO, s, l);
 return (1);
 }

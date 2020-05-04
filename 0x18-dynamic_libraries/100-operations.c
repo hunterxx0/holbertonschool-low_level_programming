e466e8d6d066b83da9802590cd61be8de61431ff
hunterxx0 <azouzimhamed@gmail.com>
2020-05-04 12:40:05 +0000

    first

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+/**
+* add(:)? (- add)?
+*
+* @a: input number
+* @b: input number
+* Return: a
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub(:)? (- sub)?
+ *
+ * @a: input number
+ * @b: input number
+ * Return: s
+ */
+int sub(int a, int b)
+{
+        return (a - b);
+}
+/**
+ * mul(:)? (- mul)?
+ *
+ * @a: input number
+ * @b: input number
+ * Return: m
+ */
+int mul(int a, int b)
+{
+        return (a * b);
+}/**
+  * div(:)? (- div)?
+  *
+  * @a: input number
+  * @b: input number
+  * Return: d
+  */
+int div(int a, int b)
+{
+        return (a / b);
+}
+/**
+ * mod(:)? (- mod)?
+ *
+ * @a: input number
+ * @b: input number
+ * Return: m
+ */
+int mod(int a, int b)
+{
+        return (a % b);
+}

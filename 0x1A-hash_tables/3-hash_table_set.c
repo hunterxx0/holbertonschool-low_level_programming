842f306c7d3a9349f304520d4c02f4db8f0d5098
hunterxx0 <azouzimhamed@gmail.com>
2020-05-29 13:07:03 +0000

    first

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,56 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get? (- retrieves a value associated with a key)?
+ *
+ * @ht: input table
+ * @k: hash key
+ * Return: n : pointer to the new hash table
+ */
+char *hash_table_get(const hash_table_t *ht, const char *k)
+{
+	unsigned long int x = 0;
+	hash_node_t *t = NULL;
+
+	if (ht)
+	{
+		while (x < ht->size)
+		{
+			if (ht->array[x])
+			{
+				if (!strcmp(ht->array[x]->key, k))
+					return (ht->array[x]->value);
+				if (ht->array[x]->next)
+				{
+					t = ht->array[x]->next;
+					while (t)
+					{
+						if (!strcmp(t->key, k))
+							return (t->value);
+						t = t->next;
+					}
+				}
+			}
+			x++;
+		}
+	}
+	return (NULL);
+}
+
 
 /**
- * check? (- Updates a hash table)?
+ * check_t? (- Updates a hash table)?
  *
  * @ht: input table
  * @k: hash key
  * @v: key's value
  * Return: n : pointer to the new hash table
  */
-int check(hash_table_t *ht, const char *k, const char *v)
+int check_t(hash_table_t *ht, const char *k, const char *v)
 {
         hash_node_t *t = NULL;
         char *z = NULL;
 
-        if (hash_table_get(ht, k))
+        if (hash_table_get(ht, k) != NULL)
         {
                 t = ht->array[key_index((const unsigned char *)k, ht->size)];
                 if (!strcmp(k, t->key))
@@ -57,7 +94,7 @@ int hash_table_set(hash_table_t *ht, const char *k, const char *v)
 
 	if (!ht || !k)
 		return (0);
-	if (check(ht, k, v))
+	if (check_t(ht, k, v))
 		return (1);
 	n = malloc(sizeof(hash_node_t));
 	if (!n)

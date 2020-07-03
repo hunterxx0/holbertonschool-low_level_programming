d8eaac72eea727b6f82a8edad095f5e2dacf9f71
hunterxx0 <azouzimhamed@gmail.com>
2020-07-03 17:11:39 +0000

    first

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,51 +4,15 @@
 
 def island_perimeter(grid):
     """Returns the perimeter of the island"""
-    wid = 0
-    leng = 0
+    if not grid:
+        return 0
+    count = res = 0
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y] == 1:
-                if ((x != len(grid)-1 and grid[x+1][y] == 1 and
-                     y != len(grid[x])-1 and grid[x][y+1] == 1) or
-                    (x != 0 and grid[x-1][y] == 1 and
-                     y != len(grid[x])-1 and grid[x][y+1] == 1) or
-                    (x != len(grid)-1 and grid[x+1][y] == 1 and
-                     y != 0 and grid[x][y-1] == 1) or
-                    (x != 0 and grid[x-1][y] == 1 and y != 0 and
-                     grid[x][y-1] == 1)):
-                    leng = 1
-                    wid = 1
-                    i, j, flag = x, y, 1
-                    while grid[i][j] == 1:
-                        if flag != 1:
-                            wid += 1
-                        if flag == 1:
-                            if (j != 0 and j != len(grid[x])-1 and
-                               grid[i][j+1] == 1 and grid[i][j-1] == 1):
-                                j -= 1
-                            elif j != 0 and grid[i][j-1] == 1:
-                                flag = 2
-                            elif j != len(grid[x])-1 and grid[i][j+1] == 1:
-                                flag = 3
-                        if flag == 3:
-                            j += 1
-                        if flag == 2:
-                            j -= 1
-                    j, flag = y, 1
-                    while grid[i][j] == 1:
-                        if flag != 1:
-                            leng += 1
-                        if flag == 1:
-                            if (i != 0 and i != len(grid[i])-1 and
-                               grid[i+1][j] == 1 and grid[i-1][j] == 1):
-                                i -= 1
-                            elif j != 0 and grid[i-1][j] == 1:
-                                flag = 2
-                            elif j != len(grid[i])-1 and grid[i+1][j] == 1:
-                                flag = 3
-                        if flag == 3:
-                            i += 1
-                        if flag == 2:
-                            i -= 1
-    return (wid * 2) + (leng * 2)
+                i, j = x, y
+                for i, j in ((i+1, j), (i-1, j), (i, j+1), (i, j-1)):
+                    if (i < 0 or i == len(grid) or j < 0 or
+                        j == len(grid[i]) or grid[i][j] == 0):
+                        count += 1
+    return count

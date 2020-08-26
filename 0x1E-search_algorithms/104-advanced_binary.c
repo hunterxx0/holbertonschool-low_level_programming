a71fdf7e2bcbba4e30ed7c87619dd1a6e7c283c7
hunterxx0 <azouzimhamed@gmail.com>
2020-08-26 19:17:45 +0000

    first

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -47,7 +47,7 @@ int _binar(int *arr, int l, int r, int val)
 		if (arr[mid] == val && arr[mid - 1] == val)
 			return (_binar(arr, l, mid, val));
 		if (arr[mid] > val)
-			return (_binar(arr, 0, mid - 1, val));
+			return (_binar(arr, l, mid - 1, val));
 		return (_binar(arr, mid + 1, r, val));
 	}
 	return (-1);
